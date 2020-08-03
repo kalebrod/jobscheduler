d777e7f55da482f43b9c8e9f9ea532f944a7bdf8
Kaleb Rodrigues <kaleb-rod@outlook.com>
2020-08-03 09:00:52 -0300

    Reaschedule falied tasks

--- app/config.py
+++ app/config.py
@@ -10,3 +10,4 @@ class Config():
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
     SCHEDULER_API_ENABLED = True
+    
--- app/utils.py
+++ app/utils.py
@@ -4,7 +4,7 @@ from datetime import datetime,timedelta
 from .extensions import db,jobmanager
 from .models import Jobs
 
-reschedule = lambda time : time + timedelta(minutes=10)
+reschedule = lambda time : time + timedelta(minutes=1)
 
 def runjob(job,attemp=1):
     print('Job %s starting ......' %job.name)
@@ -19,16 +19,34 @@ def runjob(job,attemp=1):
         update_status(job.id,'Error')
     
     if errs:
-        update_status(job.id,'Retrying')
+        if attemp > 2:
+            print('Error on Job %s. Attemp %s ,abandoning task ......' %(job.name,attemp))
+            return update_status(job.id,'Error')
+
+        print('Error on Job %s. Attemp %s ,retrying ......' %(job.name,attemp))
+        newjob = update_status(job.id,'Retrying',reschedule(job.start))
+
+        return jobmanager.add_job(
+            str(job.id),
+            func=runjob,
+            trigger='date',
+            run_date=reschedule(job.start),
+            args=[newjob,attemp+1]
+        )
 
     update_status(job.id,'Success')
 
-    print('Job %s finished......' %job.name)
+    print('Job %s finished ......' %job.name)
 
 
-def update_status(jobid,status):
+def update_status(jobid,status,time=False):
     with jobmanager.app.app_context():
-        t = Jobs.query.get(jobid)
-        t.status = status
+        temp = Jobs.query.get(jobid)
+        temp.status = status
+
+        if time:
+            temp.start = time
         
         db.session.commit()
+    
+        return Jobs.query.get(jobid)
